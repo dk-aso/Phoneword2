3e9f9528d50e88bdfa0167f2d3b24a1c1b244203
allan <allan8722@gmail.com>
2017-02-09 22:22:32 +0100

    Finish Tut

--- Phoneword2/Phoneword2.Droid/PhoneDialer.cs
+++ Phoneword2/Phoneword2.Droid/PhoneDialer.cs
@@ -25,8 +25,29 @@ namespace Phoneword2.Droid
             var context = Forms.Context;
             if (context == null)
                 return false;
-            var intent = new Intent(Intent.ActionCall);
 
+            var intent = new Intent(Intent.ActionCall);
+            intent.SetData(Uri.Parse("tel:" + number));
+
+            if (IsIntentAvailable (context, intent))
+            {
+                context.StartActivity(intent);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsIntentAvailable (Context context, Intent intent)
+        {
+            var packageManager = context.PackageManager;
+
+            var list = packageManager.QueryIntentServices(intent, 0).Union(packageManager.QueryIntentActivities(intent, 0));
+
+            if (list.Any())
+                return true;
+
+            var manager = TelephonyManager.FromContext(context);
+            return manager.PhoneType != PhoneType.None;
         }
     }
 }
---
+++ Phoneword2/Phoneword2.UWP/PhoneDialer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Phoneword2.UWP;
+using Windows.ApplicationModel.Calls;
+using Windows.UI.Popups;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword2.UWP
+{
+    public class PhoneDialer : IDialer
+    {
+        bool dialled = false;
+        public bool Dial(string number)
+        {
+            DialNumber(number);
+            return dialled;
+        }
+            async void DialNumber(string number)
+        {
+            var PhoneLine = await GetDefaultPhoneLineAsync();
+            if (PhoneLine != null)
+            {
+                PhoneLine.Dial(number, number);
+                dialled = true;
+            }
+            else
+            {
+                var dialog = new MessageDialog("No line found to place call");
+                await dialog.ShowAsync();
+                dialled = false;
+            }
+        }
+        async Task<PhoneLine> GetDefaultPhoneLineAsync()
+        {
+            var phoneCallStore = await PhoneCallManager.RequestStoreAsync();
+            var lineId = await phoneCallStore.GetDefaultLineAsync();
+            return await PhoneLine.FromIdAsync(lineId);
+        }
+
+    }
+}
